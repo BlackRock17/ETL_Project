051d671cffc8e271611389fb50e73cc8288a78db
Lyubomir <lyyubo@gmail.com>
2024-10-09 16:02:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from src.extractors.csv_extractor import extract_from_csv
 from src.extractors.api_extractor import extract_from_api
+from src.extractors.db_extractor import extract_from_db, create_sample_db
 
 
 def main():
     # Extract
     sales_df = extract_from_csv('data/sales_data.csv')
     api_data = extract_from_api('https://jsonplaceholder.typicode.com/posts')
+
+    db_path = 'data/sample.db'
+    create_sample_db(db_path)
+    customers_df = extract_from_db(db_path, 'customers')
 
     # Transform (ще добавим по-късно)
 
